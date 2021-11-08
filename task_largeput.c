0d49c9ff5ac8f94f4541fe636efe0f3aba09ca5c
RipleyTom <RipleyTom@users.noreply.github.com>
2021-11-08 14:28:33 +0100

    Add 16k PUT test

---
+++ task_largeput.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <spu_intrinsics.h>
+#include <cell/spurs.h>
+#include <cell/dma.h>
+
+CELL_SPU_LS_PARAM(16 * 1024, 16 * 1024);
+
+#define NUM_LARGE_PUTC 1000000
+
+uint8_t ls_buf[16384] __attribute__((aligned(128)));
+
+int cellSpursTaskMain(qword argTask, uint64_t argTaskset)
+{
+    const uint64_t addr = argTaskset;
+
+    for (unsigned long counter = 0; counter < NUM_LARGE_PUTC; counter++)
+    {
+        cellDmaPut(ls_buf, addr, 16384, 0, 0, 0);
+    }
+
+    return 0;
+}
---
+++ test_largeblock.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cell/spurs.h>
+#include <cell/atomic.h>
+#include "spurs_helpers.h"
+#include "globals.h"
+
+#define NUM_SPU 6
+
+unsigned char largebuf[16384] __attribute__((aligned(128)));
+
+int test_largeblock(CellSpurs2 *spurs2, const CellSpursTaskBinInfo *program)
+{
+    int ret;
+
+    CellSpursTaskset2 *taskset;
+    ret = initialize_taskset(spurs2, &taskset, (uint64_t)largebuf);
+    if (ret != 0)
+    {
+        printf("Error initializing taskset: 0x%x\n", ret);
+        return ret;
+    }
+
+    CellSpursTaskId tids[NUM_SPU];
+    CellSpursTaskArgument args[NUM_SPU];
+    void *ctx[NUM_SPU];
+
+    for (int index = 0; index < NUM_SPU; index++)
+    {
+        ctx[index] = memalign(128, program->sizeContext);
+        for (int subdex = 0; subdex < 4; subdex++)
+        {
+            args[index].u32[subdex] = (index * 10000000) + subdex;
+        }
+
+        ret = cellSpursCreateTask2WithBinInfo(taskset, &tids[index], program, &args[index], ctx[index], NULL, NULL);
+        if (ret != 0)
+        {
+            printf("Error cellSpursCreateTask2WithBinInfo: 0x%x\n", ret);
+            return ret;
+        }
+    }
+
+    int exit_code;
+    for (int index = 0; index < NUM_SPU; index++)
+    {
+        ret = cellSpursJoinTask2(taskset, tids[index], &exit_code);
+        if (ret != 0)
+        {
+            printf("Error cellSpursJoinTask2: 0x%x\n", ret);
+            return ret;
+        }
+        free(ctx[index]);
+    }
+
+    free_taskset(taskset);
+
+    return 0;
+}
