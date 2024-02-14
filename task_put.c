5e47a35bda3f6506314af489403d6d86c301e4c3
RipleyTom <RipleyTom@users.noreply.github.com>
2024-02-14 20:51:05 +0100

    Refactor PUT and Large PUT tests

--- task_put.c
+++ task_put.c
@@ -6,13 +6,15 @@
 
 CELL_SPU_LS_PARAM(16 * 1024, 16 * 1024);
 
-#define NUM_PUTC 10000000
+#define NUM_PUTC 30000000
 
 vec_uint4 ls_buf[128 / sizeof(vec_uint4)] __attribute__((aligned(128)));
 
 int cellSpursTaskMain(qword argTask, uint64_t argTaskset)
 {
-    const uint64_t addr = argTaskset;
+    const uint64_t task_id = ((vec_uint4)argTask)[0];
+    const uint64_t task_mask = 1 << task_id;
+    const uint64_t addr = argTaskset + (task_id * 128);
 
     for (int index = 0; index < (128 / sizeof(vec_uint4)); index++)
     {
@@ -26,8 +28,9 @@ int cellSpursTaskMain(qword argTask, uint64_t argTaskset)
 
     for (unsigned long counter = 0; counter < NUM_PUTC; counter++)
     {
-        cellDmaPut(&ls_buf[counter % ((128 - 32) / sizeof(vec_uint4))], addr, 32, 0, 0, 0);
+        cellDmaPut(&ls_buf[counter % ((128 - 32) / sizeof(vec_uint4))], addr, 32, task_id, 0, 0);
         ls_buf[counter % (128 / sizeof(vec_uint4))] = spu_add(ls_buf[counter % (128 / sizeof(vec_uint4))], toinc);
+        cellDmaWaitTagStatusAny(task_mask);
     }
 
     return 0;
